b69f9bf3bb02566241a1518acf4b317c0e633728
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-04-30 03:53:27 +0200

    nowe słowa kluczowe

--- pracuj/R/check_frequency.R
+++ pracuj/R/check_frequency.R
@@ -22,8 +22,10 @@ check_frequency <- function(offers, patterns = c("data sci",
                                                  "stat.st", "anality",
                                                  "analiz. danych", "excel",
                                                  "[^A-Za-z]SAS", "[^0-9][^0-9A-Za-zóÓ+]R[^0-9A-Za-zóÓęĘśŚćĆ&“”/ö][^&]",
-                                                 "big.+data", "data.+mining"),
-                            case = c(T,T,T,T,T,T,F,T,T)) {
+                                                 "big.+data", "data.+mining", "tableau", "SPSS","stata","cognos",
+                                                 "azure","aws","machine learning","spark","hadoop","matlab","scikit",
+                                                 "python","wizualizacj"),
+                            case = c(T,T,T,T,T,T,F,T,T,T,T,T,T,T,T,T,T,T,T,T,T,T)) {
   no <- sapply(seq_along(patterns), function(i){
     length(grep(offers, pattern = patterns[i], ignore.case = case[i]))
   })
